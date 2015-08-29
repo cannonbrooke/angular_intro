ce938f1589bbf537e3c69e3c1f4d39990886fbd5
Cannon Brooke <cannonbrooke@protonmail.ch>
2015-08-29 13:47:35 -1000

    working angular expressions

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + './public'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+  res.render('index.html');
+});
 
 var server = app.listen(8080);
