6e22a5d5178a0d2af1e744f06dc9ff81421dab75
Cannon Brooke <cannonbrooke@protonmail.ch>
2015-08-29 20:03:54 -1000

    working routes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,27 @@
 (function(){
-var app = angular.module('myApp', ['ngResource']);
+var app = angular.module('myApp', ['ngResource', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/books',{
+      templateUrl: 'views/books.html',
+      controller: 'BookController'
+    })
+    .when('/movies', {
+      templateUrl: 'views/movies.html',
+      controller: 'MovieController'
+    })
+    .when('/other',{
+      templateUrl: 'views/other.html',
+      controller: 'OtherController'
+    })
+    .when('/',{
+      templateUrl: 'views/default.html',
+      controller: 'myController'
+    });
+}]);
+
+
   //runs after application is loaded
   app.run(function($rootScope, APP_VERSION){
     $rootScope.APP_VERSION = APP_VERSION;
