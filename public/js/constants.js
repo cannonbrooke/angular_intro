495967884454b53f01b32157ea2402b15a5d6053
Cannon Brooke <cannonbrooke@protonmail.ch>
2015-08-29 15:19:53 -1000

    added constants

---
+++ public/js/constants.js
@@ -0,0 +1,5 @@
+(function(){
+  angular
+  .module('myApp')
+  .constant('APP_VERSION', 'v.0.0.1');
+})();
--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -1,7 +1,8 @@
 (function(){
   angular
     .module('myApp')
-    .controller('myController', ['$scope', 'myAppTitle', function($scope, myAppTitle){
+    .controller('myController', ['$scope', 'myAppTitle', 'APP_VERSION', function($scope, myAppTitle, APP_VERSION){
+      console.log(APP_VERSION);
       $scope.myModel = "Hello";
       $scope.title = myAppTitle;
   }]);
